5b89ca36a0fa578ed988aa257022f9741c1b133e
pavanV# <thoratpavan@yahoo.com>
2020-08-09 19:18:33 +0530

    Add Employee Model Class

---
+++ WebApp1/WebApp1/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp1.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+    }
+    }
+}
